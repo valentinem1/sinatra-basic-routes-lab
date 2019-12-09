7c0d612289e75581426ff96fb885721dce742964
Valentine <valentinemaillard1@gmail.com>
2019-12-09 17:05:12 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Valentine"
+    end
+
+    get '/hometown' do
+        "My hometown is Montivilliers"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Bubblin from Anderson .Paak"
+    end
+
 end
